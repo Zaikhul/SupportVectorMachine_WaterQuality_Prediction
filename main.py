ccc81594afd8c8f7647dcf35eba64794880251b7
Zaikhul Sulthon Suasono <zaikhulsulthon@gmail.com>
2024-03-06 22:00:52 +0700

    add main program

---
+++ main.py
@@ -0,0 +1,11 @@
+# .\env_ML_WaterQuality\Scripts\activate
+
+from sklearn import svm
+from sklearn import preprocessing
+from libsvm.svmutil import *
+
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('')
+
